26685f4b3f69625e8ab6e123d8d17d8b5e612965
sriraml-oup <lokesh.sriram@oup.com>
2026-05-06 14:49:33 +0530

    modified corn job enable or disable

--- src/jobs/cron.js
+++ src/jobs/cron.js
@@ -2,12 +2,14 @@ const corn = require('node-cron');
 
 const { checkApis } = require('../modules/monitoring/monitoring.service');
 
-if (process.env.CRONJOB === true) {
+const isCronEnabled = process.env.ENABLE_CRON === 'true',
+
+if (isCronEnabled) {
   corn.schedule('* * * * *', async () => {
     console.log('Running API Monitor');
 
     await checkApis();
   });
 } else {
-  console.log('Cron job disabled by env variable');
+  console.log('❌ Cron job disabled');
 }
